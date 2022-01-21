e1ca997f14a8d8d4c7fa6ff17b895fb88d90a330
nish159 <77575013+nish159@users.noreply.github.com>
2022-01-21 14:15:27 -0500

    Update User.cs

--- User.cs
+++ User.cs
@@ -2,17 +2,42 @@ namespace Models
 {
     using System;
 
+    /// <summary>
+    /// Defines a User object - the top-level representation of a user
+    /// in the system.
+    /// </summary>
     public class User
     {
+        /// <summary>
+        /// Unique document identifier.
+        /// The document identifier is used for document-based repositories and is the string
+        /// representation of the user id.
+        /// </summary>
         public string Id { get; set; }
+
+        /// <summary>
+        /// Unique user identifier used when authenticating the user
+        /// </summary>
         public string UserName { get; set; }
 
+        /// <summary>
+        /// The user's first name
+        /// </summary>
         public string FirstName { get; set; }
 
+        /// <summary>
+        /// The user's last name
+        /// </summary>
         public string LastName { get; set; }
 
+        /// <summary>
+        /// The user's password used for authentication
+        /// </summary>
         public string Password { get; set; }
 
+        /// <summary>
+        /// Creates a new istance
+        /// </summary>
         public User()
         {
             Id = Guid.NewGuid().ToString();
