64bffa8b632f9044b877019eddcbe557ce90b834
nish159 <77575013+nish159@users.noreply.github.com>
2022-01-22 10:07:36 -0500

    Update IAccountLogic.cs

--- IAccountLogic.cs
+++ IAccountLogic.cs
@@ -1,5 +1,7 @@
 namespace Logic
 {
+    using Bank;
+
     public interface IAccountLogic
     {
         /// <summary>
@@ -8,14 +10,14 @@ namespace Logic
         /// <param name="accountNumber">Indetifier of the account we want to withdraw money from</param>
         /// <param name="amount">The amount we want to withdraw from the account</param>
         /// <returns>The balance of the account after withdrawing the amount, or -1 if unable to make a withdraw</returns>
-        decimal WithdrawAmount(int accountNumber, decimal amount);
+        Result<decimal> WithdrawAmount(int accountNumber, decimal amount);
 
-        decimal WithdrawAmount(int accountNumber, string pin, decimal amount);
+        Result<decimal> WithdrawAmount(int accountNumber, string pin, decimal amount);
 
-        decimal DepositAmount(string firstName, string lastName, int accountNumber, decimal amount);
+        Result<decimal> DepositAmount(string firstName, string lastName, int accountNumber, decimal amount);
         
-        decimal DepositAmount(int accountNumber, decimal amount);
+        Result<decimal> DepositAmount(int accountNumber, decimal amount);
 
-        decimal TransferAmount(int sourceAccountNumber, string sourcePin, int destAccountNumber, string destFirstName, string destLastName, decimal amount);
+        Result<decimal> TransferAmount(int sourceAccountNumber, string sourcePin, int destAccountNumber, string destFirstName, string destLastName, decimal amount);
     }
 }
