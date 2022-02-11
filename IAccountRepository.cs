67603477b1a7b2f5c684236de2fd1bfc5ac62fc1
nish159 <77575013+nish159@users.noreply.github.com>
2022-02-11 13:58:47 -0500

    Update IAccountRepository.cs

--- IAccountRepository.cs
+++ IAccountRepository.cs
@@ -3,24 +3,22 @@ namespace DataAccess
     using System.Collections.Generic;
     using Models;
     using Bank;
+    using System.Threading.Tasks;
 
-    /// <summary>
-    /// Defines the interface for interacting with the account data store.
-    /// </summary>
     public interface IAccountRepository
     {
         /// <summary>
         /// Gets a list of all accounts in the system
         /// </summary>
         /// <returns>A list of all <see cref="Account"/>s in the system</returns>
-        Result<List<Account>> GetAllAccounts();
+        Task<Result<List<Account>>> GetAllAccountsAsync();
 
         /// <summary>
         /// Gets a list of all accounts that are belong to the user with a given user name
         /// </summary>
         /// <param name="userName">Unique identifier of the user we want to retrieve accounts for</param>
         /// <returns>A list of all <see cref="Account"/>s that belong to the user</returns>
-        Result<List<Account>> GetAllByUsername(string userName);
+        Task<Result<List<Account>>> GetAllByUsernameAsync(string userName);
 
         // function to get a single account
         /// <summary>
@@ -28,31 +26,31 @@ namespace DataAccess
         /// </summary>
         /// <param name="accountNumber">Unique account identifier</param>
         /// <returns>The <see cref="Account"/> with the given account number, or null if no account exists with that number</returns>
-        Result<Account> GetByAccountNumber(int accountNumber);
+        Task<Result<Account>> GetByAccountNumberAsync(int accountNumber);
 
         /// <summary>
         /// Gets the account with the given account id
         /// </summary>
         /// <param name="id">Unique account identifier</param>
         /// <returns>The <see cref="Account"/> with the given id, or null if no account exists with that id</returns>
-        Result<Account> GetById(string userName);
+        Task<Result<Account>> GetByIdASync(string userName);
 
         /// <summary>
         /// Creates a new account data entity
         /// </summary>
         /// <param name="account">The account to be created</param>
-        Result<Account> CreateAccount(Account account);
+        Task<Result<Account>> CreateAccountAsync(Account account);
 
         /// <summary>
         /// Updates the specified account data entity
         /// </summary>
         /// <param name="updatedAccount">The account to be updated</param>
-        Result<Account> UpdateAccount(Account updatedAccount);
+        Task<Result<Account>> UpdateAccountAsync(Account updatedAccount);
 
         /// <summary>
         /// Deletes the specified account data entity
         /// </summary>
         /// <param name="deletedAccount">The account to be deleted</param>
-        Result<Account> DeleteAccount(Account deletedAccount);
+        Task<Result<Account>> DeleteAccountAsync(Account deletedAccount);
     }
 }
