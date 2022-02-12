2b5fc7542db0b8c158965b1d97ab0c9e2f0117a5
nish159 <77575013+nish159@users.noreply.github.com>
2022-02-11 20:26:34 -0500

    Update OnlineBankingProject.cs

--- OnlineBankingProject.cs
+++ OnlineBankingProject.cs
@@ -31,7 +31,7 @@ namespace OnlineBankingProject
             Console.WriteLine("Please enter your username: ");
             userName = Console.ReadLine();
 
-            Result<BankUser> user = _userRepository.GetByUserName(userName);
+            Result<BankUser> user = _userRepository.GetByUserNameAsync(userName);
 
             if (user.Succeeded == false)
             {
@@ -154,7 +154,7 @@ namespace OnlineBankingProject
             accountNumber = int.Parse(Console.ReadLine());
 
             // Verify the account number
-            Result<Account> account = _accountRepository.GetByAccountNumber(accountNumber);
+            Result<Account> account = _accountRepository.GetByAccountNumberAsync(accountNumber).re;
 
             if (account == null)
             {
