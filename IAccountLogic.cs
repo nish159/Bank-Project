8d5f2186340119c3b00e56f47cb229b545e6120b
nish159 <77575013+nish159@users.noreply.github.com>
2023-01-24 10:07:59 -0500

    Add files via upload

---
+++ IAccountLogic.cs
@@ -0,0 +1,34 @@
+namespace Logic
+{
+    using Bank;
+    using OnlineBankingProject.Common.Models;
+    using System.Threading.Tasks;
+
+    public interface IAccountLogic
+    {
+        /// <summary>
+        /// Withdraws a given amount from a given accountNumber
+        /// </summary>
+        /// <param name="accountNumber">Indetifier of the account we want to withdraw money from</param>
+        /// <param name="amount">The amount we want to withdraw from the account</param>
+        /// <returns>The balance of the account after withdrawing the amount, or -1 if unable to make a withdraw</returns>
+        Task<Result<decimal>> WithdrawAmount(int accountNumber, decimal amount);
+
+        Task<Result<decimal>> WithdrawAmount(int accountNumber, string pin, decimal amount);
+
+        /// <summary>
+        /// Withdraws the given amount from the given account
+        /// </summary>
+        /// <param name="accountId">Identifier of the account we want to withdraw money from</param>
+        /// <param name="pin">The pin of the account the amount should be withdrawn from</param>
+        /// <param name="amount">The amount to be withdrawn from the account</param>
+        /// <returns>The account after the withdraw</returns>
+        Task<Result<Account>> WithdrawAmount(string accountId, string pin, decimal amount);
+
+        Task<Result<decimal>> DepositAmount(string firstName, string lastName, int accountNumber, decimal amount);
+        
+        Task<Result<decimal>> DepositAmount(int accountNumber, decimal amount);
+
+        Task<Result<decimal>> TransferAmount(int sourceAccountNumber, string sourcePin, int destAccountNumber, string destFirstName, string destLastName, decimal amount);
+    }
+}
