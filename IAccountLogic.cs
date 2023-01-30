fd8cd46d8c6c3d8299b8a8f543d4414dd908f37c
nish159 <77575013+nish159@users.noreply.github.com>
2023-01-30 09:03:32 -0500

    Update IAccountLogic.cs

--- IAccountLogic.cs
+++ IAccountLogic.cs
@@ -25,12 +25,12 @@ namespace Logic
         /// <returns>The account after the withdraw</returns>
         Task<Result<Account>> WithdrawAmount(string accountId, string pin, decimal amount);
 
-        Task<Result<decimal>> DepositAmount(string firstName, string lastName, int accountNumber, decimal amount);
+        Task<Result<Account>> DepositAmount(string firstName, string lastName, int accountNumber, decimal amount);
         
-        Task<Result<decimal>> DepositAmount(int accountNumber, decimal amount);
+        Task<Result<Account>> DepositAmount(int accountNumber, decimal amount);
 
         /// <summary>
-        /// Withdraws the given amount from the given account
+        /// Deposits the given amount into the account
         /// </summary>
         /// <param name="accountId">Identifier of the account we want to deposit money into</param>
         /// <param name="amount">The amount to be deposited into the account</param>
@@ -39,5 +39,16 @@ namespace Logic
         Task<Result<Account>> DepositAmount(string accountId, decimal amount, int accountNumber);
 
         Task<Result<decimal>> TransferAmount(int sourceAccountNumber, string sourcePin, int destAccountNumber, string destFirstName, string destLastName, decimal amount);
+
+        /// <summary>
+        /// Transfers the given amount from one account to another 
+        /// </summary>
+        /// <param name="id">Identifier of the account we want to transfer from</param>
+        /// <param name="sourceAccountNumber">Identifier of the account we want to transfer from</param>
+        /// <param name="sourcePin">Pin of the source account</param>
+        /// <param name="destAccountNumber">Identifier of the account we want to transfer to</param>
+        /// <param name="amount">The amount to be transferrred</param>
+        /// <returns></returns>
+        Task<Result<Account>> TransferAmount(string id, int sourceAccountNumber, string sourcePin, int destAccountNumber, decimal amount);
     }
 }
